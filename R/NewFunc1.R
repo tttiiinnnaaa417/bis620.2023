ab7792673bd281e9a0ad77b198dee829bedb0632
tttiiinnnaaa417 <152634579+tttiiinnnaaa417@users.noreply.github.com>
2023-12-19 17:57:43 -0500

    New Functions

---
+++ R/NewFunc1.R
@@ -0,0 +1,27 @@
+# (1) binary.barplot(mydata, x.vars, group.var)
+# --------------------
+# This function produces a sequence of multiple bar plots for independent variables of interest 
+# in a data frame, grouped and filled by the binary dependent variable. Each independent variable 
+# in the sequence will produce one bar plot.
+# --------------------
+#' @param mydata Name of the data frame, written in the form of mydata = name
+#' @param x.vars A combined sequence of independent variables to be plotted, 
+#' written in the form of x.vars = c("Var1", "Var2", "Var3", "...")
+#' @param group.var The binary dependent variable by which the bar plot will be grouped and filled, 
+#' written in the form of group.var = "Var"
+# --------------------
+binary.barplot <- function(mydata, x.vars, group.var) {
+  for (x.var in x.vars) {
+    p <- ggplot(mydata, aes(x = .data[[x.var]], 
+                            group = .data[[group.var]], 
+                            fill = .data[[group.var]])) +
+      geom_bar() +
+      labs(title = paste("Bar plot for", x.var, "grouped by", group.var),
+           x = x.var,
+           y = "Count",
+           fill = group.var) +
+      theme_minimal()
+    
+    print(p)
+  }
+}
---
+++ R/NewFunc2.R
@@ -0,0 +1,27 @@
+# (2) rf.imp.barplot(model, bar.color)
+# --------------------
+# This function prints out the variable importance scores in a Random Forest model based on 
+# mean decrease in accuracy, and produces a horizontal bar plot with desired bar color to 
+# visualize the variable importance.
+# --------------------
+#' @param model Name of the Random Forest model, written in the form of model = name
+#' @param bar.color The desired color of the bars, written in the form of bar.color = color
+# --------------------
+rf.imp.barplot <- function(model, bar.color) {
+  imp.feat <- importance(model, type = 1, sort = TRUE)
+  print(imp.feat)
+  
+  imp.feat <- as.array(imp.feat)
+  p <- barplot(imp.feat[,1],
+               names.arg = rownames(imp.feat),
+               main = "Variable Importance",
+               xlab = "Mean Decrease in Accuracy",
+               col = bar.color,
+               cex.names = 0.7,
+               horiz = TRUE,
+               las = 2)
+  
+  print(p)
+}
+
+
