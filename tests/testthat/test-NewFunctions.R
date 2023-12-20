ec1b3193540dff687f7bdeb97767f11ae682cbcd
tttiiinnnaaa417 <152634579+tttiiinnnaaa417@users.noreply.github.com>
2023-12-19 23:41:12 -0500

    Update

--- tests/testthat/test-NewFunctions.R
+++ tests/testthat/test-NewFunctions.R
@@ -2,17 +2,7 @@ library(testthat)
 library(ggplot2)
 library(randomForest)
 test_that("binary.barplot returns plots", {
-  expect_silent({
     data(adsl)
-    binary.barplot(adsl, c("DTH"), "PFSCR")
-  })
-})
-
-# Test for rf.imp.barplot
-test_that("rf.imp.barplot returns a barplot and prints importance", {
-  data(adsl)
-  sample_model <- randomForest(factor(PFSCR) ~ ., data = adsl[1:100,], importance = TRUE)
-  expect_output_print({
-    rf.imp.barplot(sample_model, "blue")
-  }, regexp = "Mean Decrease in Accuracy")
+    result <- binary.barplot(adsl, c("DTH"), "PFSCR")
+    expect_true(inherits(result, "ggplot"))
 })
