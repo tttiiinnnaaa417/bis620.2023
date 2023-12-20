f8eb216a5c1a14ed02b30092cbfd4aff80014edf
tttiiinnnaaa417 <152634579+tttiiinnnaaa417@users.noreply.github.com>
2023-12-19 23:01:12 -0500

    Update

--- tests/testthat/test-NewFunctions.R
+++ tests/testthat/test-NewFunctions.R
@@ -1,31 +1,15 @@
-test_that('binary.barplot returns plots', {
-  mydata <- data.frame(
-    Var1 = sample(c("A", "B"), 100, replace = TRUE),
-    Var2 = sample(c("C", "D"), 100, replace = TRUE),
-    GroupVar = sample(c(0, 1), 100, replace = TRUE)
-  )
-  plots <- binary.barplot(mydata, c("Var1", "Var2"), "GroupVar")
-  expect_true(all(sapply(plots, inherits, "gg")))
+test_that("binary.barplot returns plots", {
+  expect_silent({
+    data(adsl)
+    binary.barplot(adsl, c("DTH"), "PFSCR")
+  })
 })
 
-# Test binary.barplot handles error scenarios
-test_that('binary.barplot handles errors', {
-  mydata <- data.frame(
-    Var1 = sample(c("A", "B"), 100, replace = TRUE),
-    Var2 = sample(c("C", "D"), 100, replace = TRUE),
-    GroupVar = sample(c(0, 1), 100, replace = TRUE)
-  )
-  expect_error(binary.barplot(mydata, c("NonExistentVar"), "GroupVar"))
-})
-
-# Test rf.imp.barplot prints variable importance and returns a barplot
-test_that('rf.imp.barplot returns a barplot', {
-  mydata <- data.frame(
-    Var1 = sample(c("A", "B"), 100, replace = TRUE),
-    Var2 = sample(c("C", "D"), 100, replace = TRUE),
-    GroupVar = sample(c(0, 1), 100, replace = TRUE)
-  )
-  sample_model <- randomForest(GroupVar ~ ., data = mydata)
-  plot <- rf.imp.barplot(sample_model, "blue")
-  expect_true(inherits(plot, "gg"))
+# Test for rf.imp.barplot
+test_that("rf.imp.barplot returns a barplot and prints importance", {
+  data(adsl)
+  sample_model <- randomForest(factor(PFSCR) ~ ., data = adsl[1:100,], importance = TRUE)
+  expect_output_print({
+    rf.imp.barplot(sample_model, "blue")
+  }, regexp = "Mean Decrease in Accuracy")
 })
