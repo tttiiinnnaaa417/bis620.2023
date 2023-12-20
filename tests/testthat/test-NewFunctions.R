032ed8ad9036167f38e111cd4926a5a07b8f00f8
tttiiinnnaaa417 <152634579+tttiiinnnaaa417@users.noreply.github.com>
2023-12-20 15:58:48 -0500

    Update

--- tests/testthat/test-NewFunctions.R
+++ tests/testthat/test-NewFunctions.R
@@ -6,3 +6,10 @@ test_that("binary.barplot returns plots", {
     result <- binary.barplot(adsl, c("DTH"), "PFSCR")
     expect_true(inherits(result, "ggplot"))
 })
+
+test_that("rf.imp.barplot returns a barplot and prints importance", {
+  sample_model <- randomForest(PFSCR ~ ., data = dl)
+  expect_output_print({
+    rf.imp.barplot(sample_model, "blue")
+  }) 
+})
