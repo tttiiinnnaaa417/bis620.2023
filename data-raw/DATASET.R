daf6c80e0b618a69535b11f5e7810337cc978057
tttiiinnnaaa417 <152634579+tttiiinnnaaa417@users.noreply.github.com>
2023-12-20 16:45:07 -0500

    Update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,3 +2,12 @@
 
 accel = readRDS("accel.rds")
 usethis::use_data(accel, overwrite = TRUE)
+
+dl = readRDS("dl.rds")
+usethis::use_data(dl, overwrite = TRUE)
+
+accel = readRDS("adsl.rds")
+usethis::use_data(adsl, overwrite = TRUE)
+
+accel = readRDS("adlb.rds")
+usethis::use_data(adlb, overwrite = TRUE)
